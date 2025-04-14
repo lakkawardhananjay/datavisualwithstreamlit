206ab6ee513ebc254b05f74a4e505c7ce62b6de3
Dhananjay lakkawar <92675267+lakkawardhananjay@users.noreply.github.com>
2025-04-14 13:40:06 +0000

    version 1.1

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,8 @@ import os
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-# Initialize DynamoDBdynamodb = boto3.resource(
+# Initialize 
+DynamoDBdynamodb = boto3.resource(
     'dynamodb',
     region_name='us-east-1',  # e.g., ''
     aws_access_key_id=aws_access_key_id ,
